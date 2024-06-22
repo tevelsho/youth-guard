b0ad66b2f5a3ff5bfc959f227b930198927ef0cb
tev <tevelshorz@gmail.com>
2024-06-22 20:10:28 +0800

    feat: chatbot ui

--- app/chatbot/chatbot.jsx
+++ app/chatbot/chatbot.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import { IoChatbubblesOutline } from "react-icons/io5";
 import styles from './chatbot.module.css';
 
 export default function Chatbot() {
+    const [isChatOpen, setIsChatOpen] = useState(false);
+
+    const toggleChat = () => {
+        setIsChatOpen(!isChatOpen);
+    };
+
     return (
-        <div className={styles.chatbot}>
-            <IoChatbubblesOutline className={styles.icon} />
+        <div className={styles.chatbotContainer} onClick={toggleChat}>
+            {isChatOpen ? (
+                <div className={styles.chatWindow}>
+                    <div className={styles.chatHeader}>
+                        <span>Chat with us</span>
+                        <button className={styles.closeButton} onClick={toggleChat}>X</button>
+                    </div>
+                    <div className={styles.chatContent}>
+                        {/* Chat Stuff?? */}
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <div className={styles.chatbotIcon}>
+                        <IoChatbubblesOutline className={styles.icon} />
+                    </div>
+                </>
+            )}
         </div>
     );
 }
+
+
--- app/planner/page.jsx
+++ app/planner/page.jsx
@@ -3,7 +3,8 @@ import styles from "./page.module.css";
 
 export default function Planner(){
     return(
-        <>
-        </>
+        <section className={styles.plannerContainer}>
+
+        </section>
     );
 }
