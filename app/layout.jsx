ce58a2cde43041ed69ed9450db25be2070830f9b
braydenNP <S10258659@connect.np.edu.sg>
2024-06-28 21:24:49 +0800

    shift calc to the right

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,8 +19,8 @@ export default function RootLayout({ children }) {
           <Navbar />
           {children}
           <Chatbot />
+          <Footer className="footer" />
         </div>
-        <Footer className="footer" />
       </body>
     </html>
   );
